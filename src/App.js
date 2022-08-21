e855ac9dc0238abc1d5b4f6e5dc02ab6754d223f
karolinathorwarth@gmail.com <karolinathorwarth@web.de>
2022-08-21 20:13:45 +0200

    Try to start "emmet" -with settings.json

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap";
 
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
+import Filters from "./components/Filters/Filters";
+//!:
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters />
+          </div>
+          <div className="col-8">
+            <div className="row"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
@@ -18,3 +30,9 @@ Header.defaultProps = {
 };
 
 export default App;
+
+// After creating the component-folders, I want to import the component(s)=> create inside div-tab division "Container"-> inside "Container" div "row"-> inside "row" div with the columns I want to display in my WebPage later-->set all the Divs with a className
+// after doing so, I want to import(insert) for example the "Filter"-Component inside the div with the classname ="col-3", by opening < and writing "Filters"=> usually the path should pop up ( ./components/Filters/Filters..).
+// If it doesnt, you have to import the "Filters " manualy, by writing  on top of this file "import Filters from "./" ->propositions should be shown then
+// --> if not short-cut_ strg  / control and space
+// The import-syntax: <Filters /> ==> it is automatically importing file from src: import Filters from "./components/Filters/Filters";
