2ea9fcb6c77614b94f73db4e180a426a8c099dd8
karolinathorwarth@gmail.com <karolinathorwarth@web.de>
2022-08-21 15:28:28 +0200

    comment:connection to App.js from index or another file

--- src/App.js
+++ src/App.js
@@ -14,3 +14,5 @@ Header.defaultProps = {
 };
 
 export default App;
+
+// 3.>>> This file holds a function named App and we then export this function because as I mentioned,in modern JavaScript, if you have something,like a function or a class or an object defined in one file and you wanna use it in another file,you have to export and import it.So here in App.js where this function is defined,we export it, and then we import it (for example) in index.js,and that's how these two files are connected and how we can use a feature,in this case, a function defined in App.js inside of index.js.
