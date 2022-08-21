9f3f2f5b614596978e43964eb51cce780aebaa53
karolinathorwarth@gmail.com <karolinathorwarth@web.de>
2022-08-21 18:02:58 +0200

    installation:bootstrap, popper, paginate, router

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-// const defaultEndpoint = "https://rickandmortyapi.com/api/character";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
@@ -12,6 +10,15 @@ root.render(
   </React.StrictMode>
 );
 
-// Fetch Data from Rick and Morty endpoint:
-// 1.>>> You can just use the given endpoint(go to https://rickandmortyapi.com/documentation and chose an endpoint or take the already given one in LMS-> create a const defaultEndpoint)=> const defaultEndpoint = "https://rickandmortyapi.com/api/character";
-// 2.create function to fetch the data (given endpoint with characters)
+// Installation of following packages:
+// npx create-react-app .
+
+// npm install bootstrap
+
+// npm install @popperjs/core --save
+
+// npm install react-paginate --save
+
+// npm install react-router-dom
+
+// npm start
