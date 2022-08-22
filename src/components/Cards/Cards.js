ba6677c39bfbf2694bcf2166578410dece49767a
karolinathorwarth@gmail.com <karolinathorwarth@web.de>
2022-08-22 10:44:36 +0200

    return results of CharacterNames on the first page

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,5 +1,5 @@
 import React from "react";
-// in order to create the Cards for the Characters I came from App.js here=> paste "results" in {}into const Cards
+// in order to create the Cards for the Characters I came from App.js here=> paste "results" in {}into const Cards:const Cards = ({results}) => {
 // remove in App.js the same clo
 // now I want to .map all this results-data to Cards(in WebSite)
 // write if-statement and declare a statement, if no characters are found
@@ -11,7 +11,12 @@ const Cards = ({results}) => {
     //the x will target the results one by one
     display = results.map(x=>{
       // in order to check this, put some random word/letters between div brackets: the result will be 20 time the random letters 
-      return(<div className=" .col-4">test</div>
+      // write "col-4", because so many columns are occupied  by a singel card in the row
+      // I will use the id as the key=> so , I have to destructure it let{id}=x AND put key={id} inside the the div brackets
+      // also , I want to display the names of the characters -> destructure  this ( and put it  in the let variable next to id) and pass the id to the return function
+      // => because I wrote in App.js a default page number (1)=> I got here the results of pagenumer 1
+      let {id, name}= x
+      return(<div key= {id} className=" .col-4">{name}</div>
       )
 
     });
@@ -19,9 +24,6 @@ const Cards = ({results}) => {
     display = "Sorry, no characters found :-/";
 
   }
-
-
-  console.log('results', results);
   return <>{display}</>;
 };
 
