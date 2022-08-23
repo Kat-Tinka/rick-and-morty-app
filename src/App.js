537b88b948c273c204fbd2df8329df7c18c97e9a
karolinathorwarth@gmail.com <karolinathorwarth@web.de>
2022-08-23 14:49:09 +0200

    added functionality to SEARCH-onChange etc

--- src/App.js
+++ src/App.js
@@ -10,17 +10,22 @@ import Pagination from "./components/Pagination/Pagination";
 import Search from "./components/Search/Search";
 
 
-const App = () => {
+function App() {
   let [pageNumber, setPageNumber] = useState(1);
-
+  // 1.add FUNCTIONALITY to SEARCH:In order to add some main functionaltity to the search button; I start with a new useState (with: let [] =>and  with the variable: "search" and the function "updateSearch"):
+  //2. copy "updateSearch" and paste it in the <Search/>
+  let [search, updateSearch] = useState("");
+;
   console.log(pageNumber);
-  let[fetchedData, updateFetchedData] =useState([])
-  let{info, results}= fetchedData
+  let [fetchedData, updateFetchedData] = useState("");
+  let { info, results } = fetchedData;
 
   // console.log("fetchedData.results",fetchedData.results)
   // console.log("fetchedData.info",fetchedData.info)
-  
-  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+  // 4.add FUNCTIONALITY to SEARCH: go to API and add name PLUS use the variable, named "search" ((=> see: let [search, updateSearch] useState("");)
+  //5.add FUNCTIONALITY to SEARCH: copy functionName "updateSearch" and go to Search.js-File
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
 
   useEffect(() => {
     (async function () {
@@ -34,8 +39,14 @@ const App = () => {
   return (
     // <div className="flip-card"></div>
     <div className="App">
+    <h1 className="text-center ubuntu my-4">Rick & Morty</h1>
+
       <Header />
-      <Search/>
+    {/* 2. add FUNCTIONALITY to SEARCH: paste the copied name of the SEAR>CH Function in => <Search updateSearch/> */}
+    {/* 3.add FUNCTIONALITY to SEARCH: look up the docs of rick and Morty -> look up: Character-> gGet a single character AND Filter characters => and look up the given URL: I have to use "&name" in the API */}
+    
+      <Search updateSearch={updateSearch}/>
+
       <div className="container">
         <div className="row">
           <div className="col-3">
@@ -45,15 +56,15 @@ const App = () => {
             <div className="row">
               <div className="col-4">Cards</div>
               <Cards />
-              <Cards results={results}/>
+              <Cards results={results} />
             </div>
           </div>
         </div>
       </div>
-      <Pagination pageNumber={pageNumber} setPageNumber={setPageNumber}/>
+      <Pagination pageNumber={pageNumber} setPageNumber={setPageNumber} />
     </div>
   );
-};
+}
 
 Header.defaultProps = {
   title: "Rick and Morty App",
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import styles from './Search.module.css'
 
-const Search = () => {
+
+//6.add FUNCTIONALITY to SEARCH: coming from App.js=>  insert updateSearch in const
+
+
+const Search = ({updateSearch}) => {
   return (
     <form className='d-flex justify-content-center gap-3 mb-5'>
     <input 
+    // 7.add FUNCTIONALITY:In order to capture whatever I type in the search field , I want to capture this in the input=> I put a "onchange event" into the Search/return function"
+    onChange={event=>{
+      // 7.add FUNCTIONALITY:pass "event" into the brackets() & ".target.value"
+      // 8.add FUNCTIONALITY:=> now, whatever I write inside the input now, it will trigger the "updateSearch"-Function inside the "search" and update "search"
+      // from the:"let [search, updateSearch] = useState("");(in the App.js-file)
+      and its gonna update the API as well
+
+      updateSearch(event.target.value);
+    }}
       placeholder="Search Character" 
       type="text" 
       className={styles.input}
     />
-    <buton className="btn btn-primary">Search</buton>
-  
+    <button className={`btn btn-primary`}>Search</button>
+    {/* <button className={`{styles.btn}btn btn-primary fs-6`}>Search</button> */}
     </form>
   )
 }
